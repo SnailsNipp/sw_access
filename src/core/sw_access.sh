de160e8d00729ae2ef5c01120b1401fa0613759f
SnailsNipp <48531192+SnailsNipp@users.noreply.github.com>
2023-05-10 00:49:13 +0300

    Update sw_access.sh

--- src/core/sw_access.sh
+++ src/core/sw_access.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Prompt the user to enter an ID to search for
-#echo "Enter the ID to search for:"
-#read search_id
 # Get the id value from the command line argument
 search_id=$1
 
@@ -17,8 +14,6 @@ fi
 
 # Open a screen session and execute telnet command with the IP address
 screen -dmS telnet-session bash
-#echo -ne '\n'
-screen -r telnet-session -X stuff "Telnet session started with switch at IP $ip"$(echo -ne '\015')
+screen -r telnet-session -X stuff "telnet $ip"$(echo -ne '\015')
 #echo "Telnet session started with switch at IP $ip"
-#telnet $ip
 screen -r telnet-session
